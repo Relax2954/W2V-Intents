5a5a10c8985522e9b046b3539eab1c9995dc95b4
Relax2954 <sasa_lekic@live.com>
2019-04-08 14:08:43 +0200

    init commit

--- WTextClassification.py
+++ WTextClassification.py
@@ -21,6 +21,7 @@ sentences = [['this', 'is', 'the', 'good', 'machine', 'learning', 'book'],
              ['this', 'is', 'the', 'last', 'machine', 'learning', 'book'],
              ['orange', 'juice', 'comes', 'in', 'several', 'different', 'packages'],
              ['orange', 'juice', 'is', 'liquid', 'extract', 'from', 'fruit', 'on', 'orange', 'tree'],
+             # 2:
              ['har', 'du', 'en', 'kille'],
              ['har', 'du', 'en', 'tjej'],
              ['har', 'du', 'en', 'pojkvän'],
@@ -36,27 +37,71 @@ sentences = [['this', 'is', 'the', 'good', 'machine', 'learning', 'book'],
              ['har', 'du', 'någon', 'pojkvän', 'eller', 'flickvän'],
              ['har', 'du', 'någon', 'flickvän', 'eller', 'pojkvän'],
              ['har', 'du', 'en', 'pojkvän'],
-             ['har', 'du', 'en', 'flickvän'],
              ['har', 'du', 'nån', 'pojkvän'],
              ['har', 'du', 'nån', 'flickvän'],
-             ['har', 'du', 'nån', 'pojkvän', 'eller', 'flickvän'],
              ['har', 'du', 'nån', 'flickvän', 'eller', 'pojkvän'],
+             # 3:
              ['hur', 'mycket', 'tjänar'],
              ['vad', 'tjänar', 'du'],
              ['vad', 'har', 'du', 'i', 'lön'],
              ['vad', 'har', 'du', 'för', 'månadslön'],
              ['vad', 'har', 'du', 'i', 'månadslön'],
+
+             # 4:
+             ['vilken', 'är', 'din', 'favoriträtt'],
+             ['vilken', 'är', 'ditt', 'favoritrecept'],
+             ['vad', 'är', 'din', 'favoriträtt'],
+             ['vad', 'är', 'din', 'favoritmat'],
+             ['vad', 'är', 'ditt', 'favoritrecept'],
+             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoriträtt'],
+             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoritmat'],
+             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoritrecept'],
+             # 5:
+             ['vad', 'äter', 'du'],
+             ['är', 'du', 'vegetarian', 'vegeterian'],
+             ['är', 'du', 'vegetarian', 'vegan'],
+             # 6:
+             ['vad', 'jobbar', 'du', 'med'],
+             ['var', 'vad', 'jobbar', 'du'],
+             ['vad', 'gör', 'du', 'om', 'dagarna'],
+             ['har', 'du', 'ett', 'något', 'jobb'],
+             ['vad', 'är', 'ditt', 'jobb', 'levebröd'],
+             # 7:
+             ['är', 'du', 'en', 'man', 'eller', 'kvinna'],
+             ['är', 'du', 'en', 'kille', 'eller', 'tjej'],
+             ['är', 'du', 'en', 'man'],
+             ['är', 'du', 'en', 'kille'],
+             ['är', 'du', 'en', 'pojke'],
+             ['är', 'du', 'en', 'kvinna'],
+             ['är', 'du', 'en', 'flicka'],
+             # 8:
+             ['vad', 'heter', 'du'],
+             ['vad', 'är', 'ditt', 'namn'],
+             ['vad', 'ska', 'jag', 'kalla', 'dig'],
+             # 9:
+             ['är', 'du', 'en', 'bot'],
+             ['är', 'du', 'en', 'robot'],
+             # 10:
              ['vilken', 'är', 'din', 'favoritfärg'],
              ['vad', 'är', 'din', 'favoritfärg'],
              ['har', 'du', 'en', 'favoritfärg'],
-             ['vilken', 'är', 'din', 'favoriträtt']
-             ['vilken', 'är', 'ditt', 'favoritrecept', '']
-             ['vad', 'är', 'din', 'favoriträtt']
-             ['vad', 'är', 'din', 'favoritmat']
-             ['vad', 'är', 'ditt', 'favoritrecept']
-             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoriträtt', '']
-             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoritmat', '', '']
-             ['vilken', 'vad', 'är', 'din', 'ditt', 'favoritrecept', '']
+             # 11:
+             ['vem', 'är', 'du'],
+             # 12:
+             ['how', 'are', 'you'],
+             ['hur', 'mår', 'du'],
+             ['hej', 'hur', 'mår', 'du'],
+             ['hur', 'är', 'läget'],
+             ['läget'],
+             # testing:
+             ['har', 'du', 'en', 'flickvän'],
+             ['har', 'du', 'nån', 'pojkvän', 'eller', 'flickvän'],
+             ['vad', 'är', 'ditt', 'favoritrecept'],
+             ['är', 'du', 'en', 'pojke'],
+             ['vad', 'ska', 'jag', 'kalla', 'dig'],
+             ['vad', 'e', 'ditt', 'namn'],
+             ['vad', 'händer'],
+             ['är', 'du', 'en', 'tjej'],
              ]
 
 import numpy as np
@@ -81,18 +126,22 @@ V = []
 for sentence in sentences:
     V.append(sent_vectorizer(sentence, model))
 
-X_train = V[0:22]
-X_test = V[22:29]
+X_train = V[0:63]
+X_test = V[63:71]
 
-# 2- CatchAreYouSingle 3- CatchHowMuchDoYouMake, 4- CatchWhatsYourFavoriteColor, 5- CatchWhatsYourFavoriteFood
+# 2- CatchAreYouSingle 3- CatchHowMuchDoYouMake, 10- CatchWhatsYourFavoriteColor, 4- CatchWhatsYourFavoriteFood, 5- CatchWhatsYourDiet
+# 6-CatchWhereDoYouWork  7- CatchWhatsYourGender 8- CatchWhatsYourName 9- CatchAreYouABot 11- catchwhoareyou 12- CatchHowAreYou
 
-Y_train = [0, 0, 0, 0, 1, 1, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+Y_train = [0, 0, 0, 0, 1, 1, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+           4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9, 9, 10, 10, 10, 11, 12, 12,
+           12
+           ]
 # Y_test = [0, 1, 1]
 
 
 from sklearn.neural_network import MLPClassifier
 
-classifier = MLPClassifier(alpha=0.7, max_iter=50)
+classifier = MLPClassifier(alpha=0.7, max_iter=400)
 classifier.fit(X_train, Y_train)
 
 df_results = pd.DataFrame(data=np.zeros(shape=(1, 3)), columns=['classifier', 'train_score', 'test_score'])
